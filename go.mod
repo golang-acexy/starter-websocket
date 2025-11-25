d375b555aa1cd9429b9833903f20d420b6164a48
Acexy <thankjava@gmail.com>
2025-11-25 16:37:30 +0800

    init websocket server

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module github.com/golang-acexy/starter-websocket
 go 1.24.6
 
 require (
-	github.com/acexy/golang-toolkit v0.0.55
+	github.com/acexy/golang-toolkit v0.0.58
 	github.com/coder/websocket v1.8.14
 )
 
 require (
+	github.com/golang-acexy/starter-parent v0.1.20 // indirect
 	github.com/iancoleman/strcase v0.3.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/testify v1.11.1 // indirect
--- wsstarter/loader.go
+++ wsstarter/loader.go
@@ -1,1 +1,40 @@
 package wsstarter
+
+import (
+	"time"
+
+	"github.com/golang-acexy/starter-parent/parent"
+)
+
+type WebsocketConfig struct {
+}
+
+type WebsocketStarter struct {
+	Config           WebsocketConfig
+	LazyConfig       func() WebsocketConfig
+	config           *WebsocketConfig
+	WebsocketSetting *parent.Setting
+}
+
+func (w *WebsocketStarter) Setting() *parent.Setting {
+	if w.WebsocketSetting != nil {
+		return w.WebsocketSetting
+	}
+	return parent.NewSetting(
+		"Websocket-Starter",
+		1,
+		false,
+		time.Second*30,
+		func(instance any) {
+		})
+}
+
+func (w *WebsocketStarter) Start() (any, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (w *WebsocketStarter) Stop(maxWaitTime time.Duration) (gracefully, stopped bool, err error) {
+	//TODO implement me
+	panic("implement me")
+}
